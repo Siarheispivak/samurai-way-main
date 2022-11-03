2776ce24fb9c62a7759d4c8290020a4c5cc3d108
Siarheispivak <siarheispivak2@gmail.com>
2022-11-04 00:03:11 +0300

    refactor:split the app.tsx into components

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src="https://www.brandbucket.com/images5/gc/logo-envato.png?" alt=""/>
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Profile = () => {
+    return  (
+        <div className='content'>
+            <div>
+                <img src="https://images.pexels.com/photos/457882/pexels-photo-457882.jpeg?cs=srgb&dl=pexels-asad-photo-maldives-457882.jpg&fm=jpg" alt=""/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Profile;
