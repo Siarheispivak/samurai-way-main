720f709f5406c2c0a44f73c07be94f99face8b39
Siarheispivak <siarheispivak2@gmail.com>
2022-11-04 21:46:03 +0300

    add scss to components

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import './Navbar.css';
 
 const Navbar = () => {
     return  (
         <nav className='nav'>
-        <div><a href="">Profile</a></div>
-        <div><a href="">Messages</a></div>
-        <div><a href="">News</a></div>
-        <div><a href="">Music</a></div>
-        <div><a href="">Settings</a></div>
+        <div className='item'><a href="">Profile</a></div>
+        <div className='item'><a href="">Messages</a></div>
+        <div className='item'><a href="">News</a></div>
+        <div className='item'><a href="">Music</a></div>
+        <div className='item'><a href="">Settings</a></div>
     </nav>
     )
 }
