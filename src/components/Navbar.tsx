7d72ec51c939356d4508bd869391cbe982edcca6
Siarheispivak <siarheispivak2@gmail.com>
2022-11-11 23:08:27 +0300

    feat: add scss modules

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import './Navbar.css';
+import s from './Navbar.module.css';
 
 const Navbar = () => {
     return  (
-        <nav className='nav'>
-        <div className='item'><a href="">Profile</a></div>
-        <div className='item'><a href="">Messages</a></div>
-        <div className='item'><a href="">News</a></div>
-        <div className='item'><a href="">Music</a></div>
-        <div className='item'><a href="">Settings</a></div>
+        <nav className={s.nav}>
+        <div className={s.item}><a className={`${s.links} ${s.active}`} href="">Profile</a></div>
+        <div className={s.item}><a className={s.links} href="">Messages</a></div>
+        <div className={s.item}><a className={s.links} href="">News</a></div>
+        <div className={s.item}><a className={s.links} href="">Music</a></div>
+        <div className={s.item}><a className={s.links} href="">Settings</a></div>
     </nav>
     )
 }
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import './Profile.css';
+import s from './Profile.module.css';
 
 const Profile = () => {
     return  (
-        <div className='content'>
+        <div className={s.content}>
             <div>
                 <img src="https://images.pexels.com/photos/457882/pexels-photo-457882.jpeg?cs=srgb&dl=pexels-asad-photo-maldives-457882.jpg&fm=jpg" alt=""/>
             </div>
@@ -15,9 +15,9 @@ const Profile = () => {
                 <div>
                     New post
                 </div>
-                <div>
-                    <div>post 1</div>
-                    <div>post 2</div>
+                <div className={s.posts}>
+                    <div className={s.item}>post 1</div>
+                    <div className={s.item}>post 2</div>
                 </div>
             </div>
 
