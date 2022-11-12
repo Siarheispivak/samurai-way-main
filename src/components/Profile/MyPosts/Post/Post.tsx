e5129b27d0bbc48dd402d1b8471eac54c145853f
Siarheispivak <siarheispivak2@gmail.com>
2022-11-12 20:23:02 +0300

    rotate modules and components into folders

---
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import s from './Post.module.css';
+
+const Post = () => {
+    return  (
+               
+                    <div className={s.item}>
+                        <img src="https://cspromogame.ru//storage/upload_images/avatars/1299.jpg" alt=""/>
+                        post 1
+                    </div>
+ 
+    )
+}
+
+export default Post;
