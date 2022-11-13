f0e7547db9270c27637652528d10e9d790447f85
Siarheispivak <siarheispivak2@gmail.com>
2022-11-13 16:10:07 +0300

    refactor: rotating modules and components into folders

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import s from './MyPosts.module.css';
 import Post from "./Post/Post";
-import './MyPosts.module.css'
+import s from './MyPosts.module.css'
 
 const MyPosts = () => {
     return (
-            <div>
+        <div>
             My posts
             <div>
-            <textarea></textarea>
-            <button>Add Post</button>
-
-            <div className={s.posts}>
-                <Post />
-                <Post />
-                <Post />
+                <textarea></textarea>
+                <button>Add post</button>
             </div>
-                <div/>
-
-            )
+            <div className={s.MyPosts}>
+                <Post/>
+                <Post/>
+                <Post/>
+            </div>
+        </div>
+    )
 }
 
-export default MyPosts;
+                export default MyPosts;
--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -3,12 +3,13 @@ import s from './Post.module.css';
 
 const Post = () => {
     return  (
-               
                     <div className={s.item}>
                         <img src="https://cspromogame.ru//storage/upload_images/avatars/1299.jpg" alt=""/>
                         post 1
+                        <div>
+                            <span>like</span>
+                        </div>
                     </div>
- 
     )
 }
 
