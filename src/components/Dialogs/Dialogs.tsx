b6725ee32b22a8f8c6ab2110d664be6557b83ce8
Siarheispivak <siarheispivak2@gmail.com>
2022-11-26 03:09:44 +0300

    lesson 21 is done

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import s from 'Dialog.module.css'
+import s from './Dialog.module.css'
 
 export const Dialogs = () => {
     return (
         <div>
-            Dialogs
+            <div className={s.dialogs}>
+                <div className={s.dialogsItems}>
+                    <div className={s.dialog + ' ' + s.active }>Dimych</div>
+                    <div className={s.dialog}>Andrey</div>
+                    <div className={s.dialog}>Sveta</div>
+                    <div className={s.dialog}>Sasha</div>
+                    <div className={s.dialog}>Viktor</div>
+                </div>
+                <div className={s.messages}>
+                    <div className={s.message}>Hi</div>
+                    <div className={s.message}>How is your It-kamasutra</div>
+                    <div className={s.message}>Yo</div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import s from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return  (
         <nav className={s.nav}>
-        <div className={s.item}><a className={s.links} href="/profile">Profile</a></div>
-        <div className={s.item}><a className={`${s.links} ${s.active}`} href="/dialogs">Messages</a></div>
-        <div className={s.item}><a className={s.links} href="/news">News</a></div>
-        <div className={s.item}><a className={s.links} href="/music">Music</a></div>
-        <div className={s.item}><a className={s.links} href="/settings">Settings</a></div>
-    </nav>
+            <div className={s.item}><NavLink className={s.links} to="/profile" activeClassName={s.activeLink}>Profile</NavLink></div>
+            <div className={s.item}><NavLink className={s.links} to="/dialogs" activeClassName={s.activeLink}>Messages</NavLink></div>
+            <div className={s.item}><NavLink className={s.links} to="/news" activeClassName={s.activeLink}>News</NavLink></div>
+            <div className={s.item}><NavLink className={s.links} to="/music" activeClassName={s.activeLink}>Music</NavLink></div>
+            <div className={s.item}><NavLink className={s.links} to="/settings" activeClassName={s.activeLink}>Settings</NavLink></div>
+        </nav>
     )
 }
 
